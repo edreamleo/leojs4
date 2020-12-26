8e41d60fbd0c8da495ecf79e086fcc549b9fc9a6
Edward K. Ream <edreamleo@gmail.com>
2020-12-26 04:47:19 -0600

    Add transliterations of Leo file

---
+++ dummyLeoApp.py
@@ -0,0 +1,32 @@
+.. @+leo-ver=5-thin
+.. @+node:ekr.20201225052411.1: * @file C:\leo.repo\leojs\src\transliterations\dummyLeoApp.py
+"""
+
+leojs/src/transliterations/dummyLeoApp.py:
+
+- Driver for transcrypt transliterations of Leo's core code.
+- Test bed for the resulting JS code.
+"""
+
+from org.transcrypt.stubs.browser import console
+
+# Tell transcrypt to compile these files.
+from leo.core import leoFind
+from leo.core import leoGlobals as g
+from leo.core import leoMenu
+from leo.core import leoNodes
+from leo.core import leoUndo
+# Tell pyflakes to chill.
+assert g and leoFind and leoMenu and leoNodes and leoUndo
+
+def main():
+    console.log('dummyLeoApp.py:', __file__)
+    test()
+    
+def test():
+    g.trace('=====')
+    
+if __name__ == '__main__':
+    main()
+
+.. @-leo
