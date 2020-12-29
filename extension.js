96bcc961f3aa60c2288466e09f16bde59f5aa725
Edward K. Ream <edreamleo@gmail.com>
2020-12-29 09:31:46 -0600

    Begin new series of import tests

--- extension.js
+++ extension.js
@@ -1,35 +1,26 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-const app = require('./dummyLeoApp')
-
-// import {test} from './src/dummyLeoApp.js';
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+import {test} from './leosrc/test.mjs';
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-    // Executed ONCE!
-    console.log('===== leojs active =====');
 
-    // leojs must match "command" field in package.json
+    console.log('===== leojs active =====');   // Executed ONCE!
+
+    // extension.leojs must match "command" field in package.json
     let disposable = vscode.commands.registerCommand('extension.leojs', () => {
-        // ===== Put command code here.
-        // import main from ".src/dummyLeoApp.js"
         console.log("Before")
         vscode.window.showInformationMessage('BEFORE');
-        app.test()
+        test()
         console.log("after")
         vscode.window.showInformationMessage('AFTER');
     });
-
     context.subscriptions.push(disposable);
 }
 
-// Called when the extension is deactivated
 function deactivate() {}
 
 // eslint-disable-next-line no-undef
@@ -37,3 +28,4 @@ module.exports = {
     activate,
     deactivate
 }
+
